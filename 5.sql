1446bdf8382d885b434e9663e304e44b12a27d9b
shoichiroo <shoichiroo0601@gmail.com>
2022-07-04 10:03:33 +0000

    Update

--- 5.sql
+++ 5.sql
@@ -43,3 +43,13 @@ WHERE hanbai_tanka > (SELECT AVG(hanbai_tanka) FROM Shohin);
 
 SELECT shohin_id, shohin_mei, hanbai_tanka, (SELECT AVG(hanbai_tanka) FROM Shohin) AS avg_tanka FROM Shohin;
 
+SELECT AVG(hanbai_tanka)
+FROM Shohin
+GROUP BY shohin_bunrui;
+
+SELECT shohin_bunrui, shohin_mei, hanbai_tanka
+FROM Shohin AS S1
+WHERE hanbai_tanka > (SELECT AVG(hanbai_tanka)
+                        FROM Shohin AS S2
+                        WHERE S1.shohin_bunrui = S2.shohin_bunrui
+                        GROUP BY shohin_bunrui);
